7dd86dd6b99d34c5d2f3dfdf203556f0345517a7
João Barroso <joaobarroso@tutamail.com>
2023-07-17 08:46:40 -0300

    Added link icon.

--- include/utils/iconSize.h
+++ include/utils/iconSize.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "qsize.h"
 
-#define ICON_HEIGHT 50
-#define ICON_WIDTH 50
+#define ICON_HEIGHT          50
+#define ICON_WIDTH           50
 #define ICON_SELECTED_HEIGHT 60
-#define ICON_SELECTED_WIDTH 60
+#define ICON_SELECTED_WIDTH  60
+#define BUTTON_HEIGHT   35
+#define BUTTON_WIDTH    35
 
 const QSize iconSize(ICON_WIDTH, ICON_HEIGHT);
 const QSize iconSelectSize(ICON_SELECTED_WIDTH, ICON_SELECTED_HEIGHT);
+const QSize buttonSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
